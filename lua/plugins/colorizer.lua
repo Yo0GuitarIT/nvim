de98001acd10abe9199ab9ddda1489509712da3a
Yu Ling Chen <yo0.guitar.it@gmail.com>
2026-03-03 13:43:06 +0800

    feat: 更新 colorizer 插件的引用並重構配置，新增 nvim-tree 的檔案更新功能

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,18 +1,19 @@
 return {
-  "norcalli/nvim-colorizer.lua",
+  "NvChad/nvim-colorizer.lua",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     require("colorizer").setup({
-      "*", -- 所有檔案類型
-    }, {
-      RGB = true,      -- #RGB
-      RRGGBB = true,   -- #RRGGBB
-      names = true,    -- "blue" 等顏色名稱
-      RRGGBBAA = true, -- #RRGGBBAA
-      rgb_fn = true,   -- rgb() 和 rgba()
-      hsl_fn = true,   -- hsl() 和 hsla()
-      css = true,      -- 啟用所有 CSS 功能
-      css_fn = true,   -- 啟用所有 CSS 函數
+      filetypes = { "*" }, -- 所有檔案類型
+      user_default_options = {
+        RGB = true,      -- #RGB
+        RRGGBB = true,   -- #RRGGBB
+        names = true,    -- "blue" 等顏色名稱
+        RRGGBBAA = true, -- #RRGGBBAA
+        rgb_fn = true,   -- rgb() 和 rgba()
+        hsl_fn = true,   -- hsl() 和 hsla()
+        css = true,      -- 啟用所有 CSS 功能
+        css_fn = true,   -- 啟用所有 CSS 函數
+      },
     })
   end,
 }
--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -3,6 +3,11 @@ return {
   version = "*",
   lazy = false,
   config = function()
-    require("nvim-tree").setup()
+    require("nvim-tree").setup({
+      update_focused_file = {
+        enable = true,      -- 切換 buffer 時自動展開並定位到該檔案
+        update_root = false, -- 不自動變更根目錄
+      },
+    })
   end,
 }
