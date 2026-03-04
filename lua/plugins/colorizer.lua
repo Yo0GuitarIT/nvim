3a4d8cd1d7f3c67eb45081c3d401cf491d2a0c44
Yu Ling Chen <yo0.guitar.it@gmail.com>
2026-03-04 13:32:32 +0800

    feat: 重構 colorizer 插件配置，優化選項設置

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -4,15 +4,11 @@ return {
   config = function()
     require("colorizer").setup({
       filetypes = { "*" }, -- 所有檔案類型
-      user_default_options = {
-        RGB = true, -- #RGB
-        RRGGBB = true, -- #RRGGBB
-        names = true, -- "blue" 等顏色名稱
-        RRGGBBAA = true, -- #RRGGBBAA
-        rgb_fn = true, -- rgb() 和 rgba()
-        hsl_fn = true, -- hsl() 和 hsla()
-        css = true, -- 啟用所有 CSS 功能
-        css_fn = true, -- 啟用所有 CSS 函數
+      options = {
+        parsers = {
+          css = true,    -- 啟用所有 CSS 功能 (#RGB, #RRGGBB, #RRGGBBAA, names, rgb(), hsl())
+          css_fn = true, -- 啟用所有 CSS 函數 (rgb(), hsl(), oklch())
+        },
       },
     })
   end,
